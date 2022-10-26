f911d67a3127163936a0c51a938eed4d08808cf9
diegodl11 <diegodeluisballesteros@gmail.com>
2022-10-26 17:25:59 +0200

    fg

--- Seminario 1/seminario/BaseDatos.java
+++ Seminario 1/seminario/BaseDatos.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.sql.DriverManager;
 
 public class BaseDatos {
-    String jdbcUrl = "jdbc:oracle:thin:@//oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
+    String jdbcUrl = "jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
     String usuario = "x6512201";
     String password = "x6512201"; 
     Connection conexion;
