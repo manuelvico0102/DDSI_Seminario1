1d3bf7b350c1c8ea484a19d67215d2c2ae3617c9
diegodl11 <diegodeluisballesteros@gmail.com>
2022-10-26 16:50:27 +0200

    hola
    ads

--- Seminario 1/BaseDatos.java
+++ Seminario 1/BaseDatos.java
@@ -1,24 +1,24 @@
+package Seminario1;
 
 import java.sql.*;
 import java.sql.DriverManager;
 
-
 public class BaseDatos {
-    public BaseDatos (){
-a
-    }
     String jdbcUrl = "jdbc:oracle:thin:@//oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
     String usuario = "x6512201";
     String password = "x6512201"; 
     Connection conexion;
-    
+    public BaseDatos (){
+
+    }
     public void obtenerConexion() throws SQLException{
         try{
             Class.forName("oracle.jdbc.OracleDriver"); 
             conexion = DriverManager.getConnection(jdbcUrl, usuario, password);
-            System.out.println("se ha conectado");
+            //System.out.println("se ha conectado");
         }catch(Exception e){
             System.out.println("Error al conectar con la base de datos");
         }
     }   
 }
+
--- Seminario 1/main.java
+++ Seminario 1/main.java
@@ -1,7 +1,20 @@
+package Seminario1;
+
+
 public class main
 {
+    
     public static void main(String[ ] arg) {
+      BaseDatos base = new BaseDatos();
       System.out.println("Hola Java");
-      obtenerConexion();
-      }
+       try{
+            base.obtenerConexion();
+        }catch(Exception e){
+            System.out.println("Error al conectar con la base de datos");
+        }
+      
+  
+    }
 }
+
+
