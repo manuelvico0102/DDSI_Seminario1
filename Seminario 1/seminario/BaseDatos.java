b3147333cb3df162d503181b22e151080560002f
carlotiii30 <73278607+carlotiii30@users.noreply.github.com>
2022-10-27 11:51:09 +0200

    Update BaseDatos.java

--- Seminario 1/seminario/BaseDatos.java
+++ Seminario 1/seminario/BaseDatos.java
@@ -1,24 +1,41 @@
-
+package seminario1;
 
 import java.sql.*;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 
 public class BaseDatos {
+
     String jdbcUrl = "jdbc:oracle:thin:@//oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
-    String usuario = "x6512201";
-    String password = "x6512201"; 
+    String usuario = "x77203307";
+    String password = "x77203307"; 
     Connection conexion;
+    
     public BaseDatos (){
 
     }
+    
     public void obtenerConexion() throws SQLException{
         try{
             Class.forName("oracle.jdbc.OracleDriver"); 
             conexion = DriverManager.getConnection(jdbcUrl, usuario, password);
             //System.out.println("se ha conectado");
         }catch(Exception e){
-            System.out.println("Error al conectar con la base de datos");
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos");     
+            //System.exit(0);
         }
     }   
-}
+    
+    public void cerrarConexion(){
+        
+        try{
+            conexion.close();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error al desconectar de la base de datos");     
+            System.exit(0);
+        }
 
+    }
+    
+}
