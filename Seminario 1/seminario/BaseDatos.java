cf063e52c12e2d594562f99580184a81b3894cb7
diegodl11 <diegodeluisballesteros@gmail.com>
2022-10-27 11:53:24 +0200

    sdfs

--- Seminario 1/seminario/BaseDatos.java
+++ Seminario 1/seminario/BaseDatos.java
@@ -4,18 +4,20 @@ import java.sql.*;
 import java.sql.DriverManager;
 
 public class BaseDatos {
-    String jdbcUrl = "jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
-    String usuario = "x6512201";
-    String password = "x6512201"; 
+    String jdbcUrl = "jdbc:oracle:thin:@//oracle0.ugr.es:1521/practbd.oracle0.ugr.es";
+    String usuario = "x6520114";
+    String password = "x6520114"; 
     Connection conexion;
     public BaseDatos (){
 
     }
     public void obtenerConexion() throws SQLException{
         try{
+            System.out.println("se ha coctado");
             Class.forName("oracle.jdbc.OracleDriver"); 
+            System.out.println("se ha conectado");
             conexion = DriverManager.getConnection(jdbcUrl, usuario, password);
-            //System.out.println("se ha conectado");
+          
         }catch(Exception e){
             System.out.println("Error al conectar con la base de datos");
         }
